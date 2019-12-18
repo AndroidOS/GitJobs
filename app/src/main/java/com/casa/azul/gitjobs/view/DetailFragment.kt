d03529904c2189472149bb61106290fef2cc1fd5
AndroidOS <burrowsgit@gmail.com>
2019-12-19 09:44:06 +1100

    Added GitJob information to email intent

--- app/src/main/java/com/casa/azul/gitjobs/view/DetailFragment.kt
+++ app/src/main/java/com/casa/azul/gitjobs/view/DetailFragment.kt
@@ -4,6 +4,7 @@ package com.casa.azul.gitjobs.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.casa.azul.dogs.viewmodel.ListViewModel
 import com.casa.azul.gitjobs.R
+import com.casa.azul.gitjobs.model.GitJob
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 private const val TAG = "DetailFragment"
@@ -20,6 +22,7 @@ class DetailFragment : Fragment() {
     private var gitJobNumber = 0
     private lateinit var viewModel: ListViewModel
     private lateinit var listener: Listener
+    private lateinit var gitJob: GitJob
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,8 +43,8 @@ class DetailFragment : Fragment() {
             ViewModelProviders.of(this)[ListViewModel::class.java]
         } ?: throw Exception("Invalid Activity")
 
-        val gitJob = viewModel.getDetailJobInfo(gitJobNumber)
-        webView.loadData(gitJob?.description.toString(), "text/html; charset=UTF-8", null)
+        gitJob = viewModel.getDetailJobInfo(gitJobNumber)!!
+        webView.loadData(gitJob.description.toString(), "text/html; charset=UTF-8", null)
 
         observeViewModel()
         listener.unHideFAB(true)
@@ -62,10 +65,13 @@ class DetailFragment : Fragment() {
 
     private fun sendEmail() {
 
+        val body = gitJob.description
         val shareIntent = Intent()
         shareIntent.action = Intent.ACTION_SEND
-        shareIntent.type = "text/plain"
-        shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+        shareIntent.type = "text/html"
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, gitJob.title)
+        //shareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+        shareIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(body))
         startActivity(Intent.createChooser(shareIntent, "GitJobs"))
     }
 
