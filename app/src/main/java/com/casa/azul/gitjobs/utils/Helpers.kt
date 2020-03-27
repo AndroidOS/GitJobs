9efee30ef5a88fddba18a853f10c7cf5ca6587d5
AndroidOS <burrowsgit@gmail.com>
2020-03-27 16:16:15 +1100

    Added savePdf() to utils/Helper

--- app/src/main/java/com/casa/azul/gitjobs/utils/Helpers.kt
+++ app/src/main/java/com/casa/azul/gitjobs/utils/Helpers.kt
@@ -1,8 +1,18 @@
 package com.casa.azul.gitjobs.utils
 
 import android.content.Context
+import android.os.Environment
+import android.util.Log
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
+import com.itextpdf.text.Document
+import com.itextpdf.text.Paragraph
+import com.itextpdf.text.pdf.PdfWriter
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val TAG = "Helpers"
 
 fun saveTimeStampPrefs(time: Long, context: Context){
     val TIME_STAMP = "time_stamp"
@@ -12,3 +22,43 @@ fun saveTimeStampPrefs(time: Long, context: Context){
     }
 }
 
+fun savePdf() {
+    //create object of Document class
+    val mDoc = Document()
+    //pdf file name
+    val mFileName = SimpleDateFormat(
+        "yyyyMMdd_HHmmss",
+        Locale.getDefault()
+    ).format(System.currentTimeMillis())
+    //pdf file path
+    val mFilePath =
+        Environment.getExternalStorageDirectory().toString() + "/" + mFileName + ".pdf"
+    try {
+        //create instance of PdfWriter class
+        PdfWriter.getInstance(mDoc, FileOutputStream(mFilePath))
+
+        //open the document for writing
+        mDoc.open()
+
+        //get text from EditText i.e. textEt
+        val mText = "Test string"
+        //add author of the document (metadata)
+        mDoc.addAuthor("Manuel Carvalho")
+
+        //add paragraph to the document
+        mDoc.add(Paragraph(mText))
+
+        //close document
+        mDoc.close()
+
+        //show file saved message with file name and path
+        //Toast.makeText(this, "$mFileName.pdf\nis saved to\n$mFilePath", Toast.LENGTH_SHORT)
+        // .show()
+    } catch (e: Exception) {
+        //if anything goes wrong causing exception, get and show exception message
+        Log.d(TAG, e.message)
+        //Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+    }
+
+
+}
--- app/src/main/java/com/casa/azul/gitjobs/view/MainActivity.kt
+++ app/src/main/java/com/casa/azul/gitjobs/view/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.casa.azul.gitjobs.view
 
+import android.Manifest
 import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
@@ -11,6 +15,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.casa.azul.dogs.viewmodel.ListViewModel
 import com.casa.azul.gitjobs.R
+
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), Listener {
@@ -18,10 +24,33 @@ class MainActivity : AppCompatActivity(), Listener {
     private lateinit var viewModel: ListViewModel
     private lateinit var navController: NavController
 
+    private val STORAGE_CODE: Int = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            //system OS >= Marshmallow(6.0), check permission is enabled or not
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED
+            ) {
+                //permission was not granted, request it
+                val permissions = arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                requestPermissions(permissions, STORAGE_CODE)
+            } else {
+                //permission already granted, call savePdf() method
+                com.casa.azul.gitjobs.utils.savePdf()
+            }
+        } else {
+            //system OS < marshmallow, call savePdf() method
+//                savePdf()
+        }
+
 
         navController = Navigation.findNavController(this, R.id.fragment)
 
@@ -78,6 +107,24 @@ class MainActivity : AppCompatActivity(), Listener {
             fab.show()
         } else {
             fab.hide()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            STORAGE_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup was granted, call savePdf() method
+                    //savePdf()
+                } else {
+                    //permission from popup was denied, show error message
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
